3e48390b888b3ee47ef5896ab6179290b11a097a
Tiago Rodrigues <mail@tig.pt>
2024-05-24 22:30:15 +0100

    remove unused code

--- terraform/route53_awsug.site.tf
+++ terraform/route53_awsug.site.tf
@@ -1,8 +1,4 @@
-#Root name server for the public domain.
-resource "aws_route53_zone" "awsug_site" {
-  name = "awsug.site"
-}
-
+# Hosted zone for awsug.site
 module "route53_awsug_zones" {
   source  = "terraform-aws-modules/route53/aws//modules/zones"
   version = "~> 2.0"
