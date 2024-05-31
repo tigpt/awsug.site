e12645740c6a516077833875cc479cfad765ada3
Tiago Rodrigues <mail@tig.pt>
2024-05-31 22:59:20 +0100

    refactor some code to use locals

---
+++ terraform/city.awsug.site.tf
@@ -0,0 +1,16 @@
+module "route53_awsug_records" {
+  source   = "terraform-aws-modules/route53/aws//modules/records"
+  version  = "~> 2.0"
+  for_each = local.usergroup
+
+  zone_name = "awsug.site"
+
+  records = [
+    {
+      name    = each.value.city
+      type    = "NS"
+      ttl     = 172800
+      records = each.value.nameservers
+    }
+  ]
+}
---
+++ terraform/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  usergroup = {
+    lisbon = {
+      city = "lisbon"
+      nameservers = [
+        "ns-269.awsdns-33.com.",
+        "ns-1702.awsdns-20.co.uk.",
+        "ns-1393.awsdns-46.org.",
+        "ns-595.awsdns-10.net."
+      ]
+    }
+  }
+}
