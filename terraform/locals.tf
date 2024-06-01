6cadcdb3f42bcc95fdb6e83f26407088283aad75
Tiago Rodrigues <mail@tig.pt>
2024-06-01 08:39:35 +0100

    add comments to locals

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -7,6 +7,21 @@ locals {
         "ns-1393.awsdns-46.org.",
         "ns-595.awsdns-10.net."
       ]
-    }
+    },
+    #
+    # To create the subodmain for your city, copy this block bellow and change it to reflect your city configurations. If you dont' know how to start, look at lisbon github terraform to see how to create a hostzone in the url bellow:
+    # https://github.com/tigpt/lisbon.awsug.site
+    #
+    your_city = {
+      nameservers = [
+        "yours-1.hostzone-nameserver-here.com.",
+        "yours-2.hostzone-nameserver-here.com.",
+        "yours-<3.hostzone-nameserver-here.com.",
+        "yours-4.hostzone-nameserver-here.com."
+      ]
+    },
+    #
+    # end of the template block
+    #
   }
 }
